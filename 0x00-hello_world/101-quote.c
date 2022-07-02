31b769ec6bd36e7553aea0a46751372b9efb63dc
Blessedwonder <blessedokotie@gmail.com>
2022-07-01 17:27:07 -0700

    101 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include<stdio.h>                                                                              
+#include <stdio.h>                                                                              
 #include <unistd.h>
 
 /**
  * *main - print exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
- *   follwed by a new line, to standard error.
- *   return: Always 0 (success)
+ *   followed by a new line,to standard error.
+ *   Return: Always 0 (success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - dora korpar, 2015-10-199\n", 59);
+write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 return (1);
 }
