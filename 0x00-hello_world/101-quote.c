8dc16dc6b504f2d967b399e4027d3d051723c4e6
Blessedwonder <blessedokotie@gmail.com>
2022-07-01 17:45:02 -0700

    101 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
-#include <stdio>                                                              
+#include <stdio.h>
+#include <unistd.h>                                                              
 /**
- * *main - print exactly "and that piece of art is useful"
+ * *main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
- *   followed by a new line,to standard error.
- *   Return: Always 0 (success)
+ * followed by a new line,to standard error.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
