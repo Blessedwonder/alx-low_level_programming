b45bb7b2f8dcbc70b463bb5c32154c6a787dd65f
Blessedwonder <blessedokotie@gmail.com>
2022-07-03 16:14:27 -0700

    0 commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,34 @@
-#include <stdlib.h>                                                                                                             
-#include <time.h>                                                                                                               
-#include <stdio.h>                                                                                                              
-                                                                                                                                
-/**                                                                                                                             
- *  * main - Entry point                                                                                                           
- *  * based a condition                                                                                                            
- *  * Return : Always 0 (sucess/correct)                                                                                           
- * */                                                                                                                              
-int main(void)                                                                                                                  
-{                                                                                                                               
-int n;                                                                                                                          
-		                                                                                                                                
-srand(time(0));                                                                                                                 
-n = rand() - RAND_MAX / 2;                                                                                                      
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
 
-if (n == 0)                                                                                                                     
-{                                                                                                                               
-printf("%d is zero\n", n);
+/**
+ * main - Entry point
+ *
+ * Return: Always (success/correct)
+ */
+
+int main(void)
+{
+int n,
+
+sran(time(0));
+n=rand() - RAND_MAX / 2;
+
+if (n == 0)
+{
+printf("%i is zero\n",n);
 }
 
-else if (n < 0)                                                                                                                 
-{                                                                                                                               
-printf("%d is negative\n", n);                                                                                                  
+else if (n < 0)
+{
+printf("%i is negative\n",n);
 }
-                                                                                                                                else                                                                                                                     
-{          
-printf("%d is positive\n", n);                                                                                                  
-}                                                                                                                               
-return (0);                                                                                                                     
-}           
 
+else
+{
+printf("%i is positive\n",n);
+}
+
+return (0);
+}
