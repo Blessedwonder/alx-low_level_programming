7a0571e831013db3228e359b38f1394848140e3e
Blessedwonder <blessedokotie@gmail.com>
2022-07-03 15:02:21 -0700

    0 commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /**
- * main -> assign a random number to the variable n each time it is excuted and prints out
+ * main -> assign a random number to the variable n each time it is excuted 
+ and prints out
  * based a condition
  * Return : always 0
 */
